b7dac1aa3c671ab25a11b4cab88346b3a1afd049
Jakub <jurak2012@gmail.com>
2021-10-17 12:33:17 +0200

    Generation type - Table

--- src/main/java/com/crud/library/domain/Reader.java
+++ src/main/java/com/crud/library/domain/Reader.java
@@ -18,7 +18,7 @@ import java.util.List;
 public class Reader {
 
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @GeneratedValue(strategy=GenerationType.TABLE)
     @NotNull
     @Column(name = "id", unique = true)
     private Long id;
